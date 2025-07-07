076fe01b47800c5ef10c23b65fb1717423b1feec
Nina <NTonkova@IBS.RU>
2025-07-08 01:17:52 +0500

    Gitignore added

---
+++ backend/admin.py
@@ -0,0 +1,76 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from backend.models import Category, User, Shop, Product, Contact,ProductInfo, Parameter, ProductParameter, Order, OrderItem, \
+    ConfirmEmailToken
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    model = User
+
+    fieldsets = (
+        (None, {'fields': ('email','type', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'company', 'position')}),
+        ('Permissions'),
+         {
+            'fields': (
+                'is_active',
+                'is_superuser',
+                'is_staff',
+                'is superuser'
+                'groups',
+                'user_permissions',
+            ),
+        },
+        (('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ('email', 'last_name', 'first_name', 'is_staff')
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProductInfo)
+class ProductInfoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Parameter)
+class ParameterAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProductParameter)
+class ProductParameterAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ConfirmEmailToken)
+class ConfirmEmailTokenAdmin(admin.ModelAdmin):
+    list_display = ('user', 'created_at', 'key')
---
+++ backend/exception.py
@@ -0,0 +1,12 @@
+from rest_framework.exceptions import APIException
+
+class ItemNotFound(APIException):
+    status_code = 404
+    default_detail = 'Item Not found'
+    default_code = 'Incorrect data'
+
+
+class InvalidActivationCode(APIException):
+    status_code = 404
+    default_detail = 'Activation link is invalid'
+    default_code = 'Incorrect data'
