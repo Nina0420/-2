93dc5584bdeac8e35996ecd1df4320f6eec56858
Nina0420 <119350776+Nina0420@users.noreply.github.com>
2025-07-09 00:27:33 +0500

    signals.py

--- backend/signals.py
+++ backend/signals.py
@@ -12,8 +12,6 @@ new_user_registered = Signal(
 new_order = Signal(
     providing_args=['user_id'],
 )
-
-
 @receiver(reset_password_token_created)
 def password_reset_token_created(sender, instance=None, reset_password_token, **kwargs):
 
@@ -27,34 +25,24 @@ def password_reset_token_created(sender, instance=None, reset_password_token, **
 
 @receiver(new_user_registered)
 def new_user_registered_signal(user_id, **kwargs):
-    # send an e-mail to the user
     token, _ = ConfirmEmailToken.objects.get_or_create(user_id=user_id)
 
     msg = EmailMultiAlternatives(
-        # title:
         f"Password Reset Token for {token.user.email}",
-        # message:
         token.key,
-        # from:
         settings.EMAIL_HOST_USER,
-        # to:
         [token.user.email]
     )
     msg.send()
 
 @receiver(new_order)
 def new_order_signal(user_id, **kwargs):
-    # send an e-mail to the user
     user = User.objects.get(id=user_id)
 
     msg = EmailMultiAlternatives(
-        # title:
         f"Обновление статуса заказа",
-        # message:
         'Заказ сформирован',
-        # from:
         settings.EMAIL_HOST_USER,
-        # to:
         [user.email]
     )
     msg.send()
